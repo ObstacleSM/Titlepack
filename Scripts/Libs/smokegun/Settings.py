a99288bfc58029d8329e63f5e9b54bddc29e84af
Vincent Parizet <vincent.parizet@hotmail.fr>
2019-07-26 18:00:50 +0200

    Add settings to force invisible skin and highlight

--- Scripts/Libs/smokegun/Settings.py
+++ Scripts/Libs/smokegun/Settings.py
@@ -1,6 +1,6 @@
 model = {
 	'Integers': ['TimeLimit', 'RoundLimit', 'MinPlayers', 'PointLimit', 'PointsMax', 'PointsMin', 'PointsDeduced', 'RoundCooldown', 'WarmupRounds', 'MinTimeLeft', 'MaxTimeLeft'],
-	'Booleans': ['PvPWeapons', 'FastRespawn', 'AllowJump', 'EnableRecords', 'LegacyRespawn', 'FullAmmoSpawn', 'PointDistribution'],
+	'Booleans': ['PvPWeapons', 'FastRespawn', 'AllowJump', 'EnableRecords', 'LegacyRespawn', 'FullAmmoSpawn', 'PointDistribution', 'ForceInvisibleSkin', 'EnablePlayerHighlight'],
 	'Reals': ['NextMapVotingRatio'],
 	'Texts': ['BestNewRecordMsg', 'WorseNewRecordMsg']
 }
