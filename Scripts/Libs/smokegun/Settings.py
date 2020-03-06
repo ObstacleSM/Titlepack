89bd6683c7275b1ce66b33dd2e506f22115300f7
Vincent Parizet <vincent.parizet@hotmail.fr>
2020-03-06 17:40:51 +0100

    survival mode + fix records

--- Scripts/Libs/smokegun/Settings.py
+++ Scripts/Libs/smokegun/Settings.py
@@ -1,5 +1,5 @@
 model = {
-	'Integers': ['TimeLimit', 'RoundLimit', 'MinPlayers', 'PointLimit', 'PointsMax', 'PointsMin', 'PointsDeduced', 'RoundCooldown', 'WarmupRounds', 'MinTimeLeft', 'MaxTimeLeft'],
+	'Integers': ['TimeLimit', 'ScoreMode', 'RoundLimit', 'MinPlayers', 'PointLimit', 'PointsMax', 'PointsMin', 'PointsDeduced', 'RoundCooldown', 'WarmupRounds', 'MinTimeLeft', 'MaxTimeLeft'],
 	'Booleans': ['PvPWeapons', 'FastRespawn', 'AllowJump', 'EnableRecords', 'LegacyRespawn', 'FullAmmoSpawn', 'PointDistribution', 'ForceInvisibleSkin', 'EnablePlayerHighlight'],
 	'Reals': ['NextMapVotingRatio'],
 	'Texts': ['BestNewRecordMsg', 'WorseNewRecordMsg']
