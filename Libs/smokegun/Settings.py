7afd8f83660bb6967249aad5e980c6a5bfb4f3a2
Baptiste Marsigli <baptistemarsigli@gmail.com>
2021-07-04 18:53:30 +0200

    I hate me

--- Libs/smokegun/Settings.py
+++
@@ -1,6 +0,0 @@
-model = {
-	'Integers': ['TimeLimit', 'ScoreMode', 'RoundLimit', 'MinPlayers', 'PointLimit', 'PointsMax', 'PointsMin', 'PointsDeduced', 'RoundCooldown', 'WarmupRounds', 'MinTimeLeft', 'MaxTimeLeft', 'CheckpointMarkersCount'],
-	'Booleans': ['PvPWeapons', 'FastRespawn', 'AllowJump', 'EnableRecords', 'LegacyRespawn', 'FullAmmoSpawn', 'PointDistribution', 'ForceInvisibleSkin', 'EnablePlayerHighlight'],
-	'Reals': ['NextMapVotingRatio'],
-	'Texts': ['BestNewRecordMsg', 'WorseNewRecordMsg', 'ForceRNGSeed']
-}
