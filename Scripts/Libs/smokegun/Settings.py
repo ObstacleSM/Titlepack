e929cfb0a1661bd7f35461942baee37341e119d3
Miltant <baptistemarsigli@gmail.com>
2023-01-08 19:53:42 +0100

    1.2.1

--- Scripts/Libs/smokegun/Settings.py
+++ Scripts/Libs/smokegun/Settings.py
@@ -1,5 +1,5 @@
 model = {
-	'Integers': ['TimeLimit', 'ScoreMode', 'RoundLimit', 'MinPlayers', 'PointLimit', 'PointsMax', 'PointsMin', 'PointsDeduced', 'RoundCooldown', 'WarmupRounds', 'MinTimeLeft', 'MaxTimeLeft', 'CheckpointMarkersCount'],
+	'Integers': ['TimeLimit', 'ScoreMode', 'RoundLimit', 'MinPlayers', 'PointLimit', 'PointsMax', 'PointsMin', 'PointsDeduced', 'RoundCooldown', 'WarmupRounds', 'GetWarmupRoundsTime', 'MinTimeLeft', 'MaxTimeLeft', 'CheckpointMarkersCount'],
 	'Booleans': ['FastRespawn', 'AllowJump', 'EnableRecords', 'LegacyRespawn', 'FullAmmoSpawn', 'PointDistribution', 'ForceInvisibleSkin', 'EnablePlayerHighlight'],
 	'Reals': ['NextMapVotingRatio'],
 	'Texts': ['BestNewRecordMsg', 'WorseNewRecordMsg', 'ForceRNGSeed']
